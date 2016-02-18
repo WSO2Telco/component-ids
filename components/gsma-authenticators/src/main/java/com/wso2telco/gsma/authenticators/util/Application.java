b5d7f9d4d92b119f5bff5e12596944a046bc9e0b
sheshan <sheshan@wso2telco.com>
2016-02-18 16:25:08 +0530

    Added Apache logins according to user story PRODEV-171 gsma-authenticators - standardize

--- components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/util/Application.java
+++ components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/util/Application.java
@@ -38,7 +38,7 @@ public class Application {
         try {
             applicationName = applicationName.substring(applicationName.indexOf("_") + 1);
         }catch(Exception ex){
-            log.info("application name not convert");
+            log.error("application name not convert" +ex);
         }
         return applicationName;
     }
