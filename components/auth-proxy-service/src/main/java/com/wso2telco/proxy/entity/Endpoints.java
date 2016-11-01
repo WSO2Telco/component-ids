b4a0b232acd866a582b2e814a4506bec3117c7da
lakshani <gamagelakshani@gmail.com>
2016-11-01 23:48:25 +0530

    added licence to java classes

--- components/auth-proxy-service/src/main/java/com/wso2telco/proxy/entity/Endpoints.java
+++ components/auth-proxy-service/src/main/java/com/wso2telco/proxy/entity/Endpoints.java
@@ -1,7 +1,18 @@
-/*
- *   To change this template, choose Tools | Templates
- *   and open the template in the editor.
- */
+/*******************************************************************************
+ * Copyright  (c) 2015-2016, WSO2.Telco Inc. (http://www.wso2telco.com) All Rights Reserved.
+ *
+ * WSO2.Telco Inc. licences this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package com.wso2telco.proxy.entity;
 
 import com.google.gdata.util.common.util.Base64DecoderException;
@@ -142,7 +153,7 @@ public class Endpoints {
 
                 // Replace query parameters based on the scope.
                 queryParams.putSingle(AuthProxyConstants.SCOPE, AuthProxyConstants.SCOPE_OPENID);
-                queryParams.putSingle(AuthProxyConstants.ACR, AuthProxyConstants.ACR_CPI_VALUE);
+                queryParams.putSingle(AuthProxyConstants.ACR, "6");
                 queryString = processQueryString(queryParams, queryString);
 
             } else if (operatorScope.equals(AuthProxyConstants.SCOPE_MNV)) {
