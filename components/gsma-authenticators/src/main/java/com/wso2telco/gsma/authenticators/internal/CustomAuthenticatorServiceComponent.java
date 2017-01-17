3f98679af1349d8231ddeb8d0cb13f2fe847d893
madushak <poosathecat@gmail.com>
2017-01-17 11:00:35 +0530

    Refactoring

--- components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/internal/CustomAuthenticatorServiceComponent.java
+++ components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/internal/CustomAuthenticatorServiceComponent.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
+
 package com.wso2telco.gsma.authenticators.internal;
 
 import com.wso2telco.gsma.authenticators.*;
