3371b03690af06a20c86f2638a97d1a582a202d2
Isuru Ranaweera <isurur@wso2telco.com>
2017-01-27 14:48:16 +0530

    Fixed javadoc issues, replaced ReadMobileConnectConfig usage with configurationService

--- components/ids-data-publisher/src/main/java/com/wso2telco/ids/datapublisher/internal/DataPublisherServiceComponent.java
+++ components/ids-data-publisher/src/main/java/com/wso2telco/ids/datapublisher/internal/DataPublisherServiceComponent.java
@@ -18,9 +18,6 @@ package com.wso2telco.ids.datapublisher.internal;
 
 import org.osgi.service.component.ComponentContext;
 
-/**
- *@scr.component name="ids.agent" immediate="true"
- */
 public class DataPublisherServiceComponent {
 
     protected void activate(ComponentContext context) {
