07d31aa177737c44302d49d1ac47437561e65c44
madushak <poosathecat@gmail.com>
2017-04-27 18:48:25 +0530

    Added angular app to component - IDSDEV-379

---
+++ components/self-service-portal/src/angular-source/src/app/pages/pages.menu.ts
@@ -0,0 +1,19 @@
+export const PAGES_MENU = [
+  {
+    path: 'pages',
+    children: [
+      {
+        path: 'dashboard',
+        data: {
+          menu: {
+            title: 'Dashboard',
+            icon: 'ion-android-home',
+            selected: false,
+            expanded: false,
+            order: 0
+          }
+        }
+      }
+    ]
+  }
+];
