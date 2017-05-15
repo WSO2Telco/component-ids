95491e9a10f8148f7fd6e4a7ebaf5e49e9b042cd
madushak <poosathecat@gmail.com>
2017-05-15 15:01:53 +0530

    PIN reset UI - IDSDEV-557

--- components/self-service-portal/src/angular-source/src/app/pages/register/register.component.ts
+++ components/self-service-portal/src/angular-source/src/app/pages/register/register.component.ts
@@ -1,8 +1,10 @@
 import {Component} from '@angular/core';
 import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/forms';
 import {EmailValidator, EqualPasswordsValidator} from '../../theme/validators';
+import {Http, Response} from '@angular/http';
 
 import 'style-loader!./register.scss';
+import {AppSettings} from "../../app.settings";
 
 @Component({
   selector: 'register',
@@ -11,37 +13,41 @@ import 'style-loader!./register.scss';
 export class Register {
 
   public form:FormGroup;
-  public name:AbstractControl;
-  public email:AbstractControl;
-  public password:AbstractControl;
-  public repeatPassword:AbstractControl;
-  public passwords:FormGroup;
+  public current:AbstractControl;
+  public newPin:AbstractControl;
+  public repeatNewPin:AbstractControl;
+  public newPins:FormGroup;
 
   public submitted:boolean = false;
 
-  constructor(fb:FormBuilder) {
+  constructor(fb:FormBuilder, private http: Http) {
 
     this.form = fb.group({
-      'name': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
-      'email': ['', Validators.compose([Validators.required, EmailValidator.validate])],
-      'passwords': fb.group({
-        'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
-        'repeatPassword': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
-      }, {validator: EqualPasswordsValidator.validate('password', 'repeatPassword')})
+      'current': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      'newPins': fb.group({
+        'newPin': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+        'repeatNewPin': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
+      }, {validator: EqualPasswordsValidator.validate('newPin', 'repeatNewPin')})
     });
 
-    this.name = this.form.controls['name'];
-    this.email = this.form.controls['email'];
-    this.passwords = <FormGroup> this.form.controls['passwords'];
-    this.password = this.passwords.controls['password'];
-    this.repeatPassword = this.passwords.controls['repeatPassword'];
+    this.current = this.form.controls['current'];
+    this.newPins = <FormGroup> this.form.controls['newPins'];
+    this.newPin = this.newPins.controls['newPin'];
+    this.repeatNewPin = this.newPins.controls['repeatNewPin'];
   }
 
   public onSubmit(values:Object):void {
     this.submitted = true;
     if (this.form.valid) {
-      // your code goes here
-      // console.log(values);
+      let current_pin = values.current;
+      let new_pin = values.newPins.newPin;
+      let token = localStorage.getItem('access_token');
+
+      this.http.get(AppSettings.BASE_API + 'user/pin_reset?access_token='+ token + '&current=' + current_pin + '&new_pin=' + new_pin).map((response:Response) => {
+                console.log(response.json());
+                // handle response here
+                //response.json();
+            }).subscribe();
     }
   }
 }
