e58cb6cc9328f5abd8594675684bdc417a5cba61
madushak <poosathecat@gmail.com>
2017-05-19 15:22:34 +0530

    Added login summary chart and pin reset IDSDEV-557

--- components/self-service-portal/src/angular-source/src/app/auth/activator.ts
+++ components/self-service-portal/src/angular-source/src/app/auth/activator.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class ProtectedRouteCanActivate implements CanActivate {
@@ -9,6 +11,7 @@ export class ProtectedRouteCanActivate implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate() {
-     return true; //this.authService.isLoggedIn();
+    //return true;
+    return this.authService.isLoggedIn();
   }
 }
