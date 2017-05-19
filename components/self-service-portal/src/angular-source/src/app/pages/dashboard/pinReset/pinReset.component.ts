b61644c89d1333af6970bbdd4a09a0c0a04347a6
madushak <poosathecat@gmail.com>
2017-05-19 15:22:34 +0530

    Added login summary chart and pin reset IDSDEV-557

---
+++ components/self-service-portal/src/angular-source/src/app/pages/dashboard/pinReset/pinReset.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import 'style-loader!./pinReset.scss';
+
+@Component({
+  selector: 'pin-reset',
+  templateUrl: './pinReset.html'
+})
+export class PinReset {
+
+  ngOnInit() {
+  }
+}
