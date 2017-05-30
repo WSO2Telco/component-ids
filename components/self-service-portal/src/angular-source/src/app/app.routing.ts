448c23bff846236e6ea8058070983646dc01dc41
madushak <poosathecat@gmail.com>
2017-05-30 11:43:43 +0530

    Added build scripts and hashed pin reset  - IDSDEV-557

--- components/self-service-portal/src/angular-source/src/app/app.routing.ts
+++ components/self-service-portal/src/angular-source/src/app/app.routing.ts
@@ -3,7 +3,6 @@ import { ModuleWithProviders } from '@angular/core';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: '*', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'pages/dashboard' }
 ];
 
