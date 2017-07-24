30478d60417d6822860db08510eecce08c62861b
Ruban <mahadeva@wso2telco.com>
2017-07-24 16:10:49 +0530

    Scope Validation Changes, LOA Level Fix, User Info Fix

---
+++ components/session-updater/src/main/java/com/wso2telco/scopevalidation/OutboundMessage.java
@@ -0,0 +1,15 @@
+package com.wso2telco.scopevalidation;
+
+public class OutboundMessage {
+
+	private ScopeValidationResponse scopeValidationResponse;
+
+	public ScopeValidationResponse getScopeValidationResponse() {
+		return scopeValidationResponse;
+	}
+
+	public void setScopeValidationResponse(ScopeValidationResponse scopeValidationResponse) {
+		this.scopeValidationResponse = scopeValidationResponse;
+	}
+
+}
