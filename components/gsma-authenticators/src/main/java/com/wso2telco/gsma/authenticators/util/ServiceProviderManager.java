811c5dd3ae0df9d1ee3f2ea1d6afc0aa2a37f2eb
aushaniU <aushani@wso2telco.com>
2017-07-24 12:08:48 +0530

    [IDSDEV-620]Create new class for implement the SP's spesific things

---
+++ components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/util/ServiceProviderManager.java
@@ -0,0 +1,18 @@
+package com.wso2telco.gsma.authenticators.util;
+
+/**
+ * Create on 7/24/17.
+ */
+public class ServiceProviderManager {
+
+
+    public String checkSPType(){
+
+        return "sp";
+    }
+
+    public String checkConsentModel() {
+
+        return "consent";
+    }
+}
