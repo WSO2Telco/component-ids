7e3692b1fb3eabb783e83ed02d6342e236f413e5
nilan <nilan.rukshan@gmail.com>
2017-08-01 10:13:21 +0530

    IDSDEV-592

--- components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/Constants.java
+++ components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/Constants.java
@@ -417,7 +417,7 @@ public final class Constants {
     public static final String SCOPE = "scope";
 
     public static final String PROMPT = "telco_prompt";
-    
+
     public static final String OPERATOR_ID = "operatorID";
 
     public static final String ACTIVE = "ACTIVE";
