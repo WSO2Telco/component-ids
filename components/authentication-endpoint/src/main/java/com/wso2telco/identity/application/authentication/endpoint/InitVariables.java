01a778b3b223bcc3118e54ee70aef947db440119
rshahintha <shahintha@wso2telco.com>
2017-08-04 18:35:42 +0530

    IDSDEV-639 : Language config read from m-c.xml

--- components/authentication-endpoint/src/main/java/com/wso2telco/identity/application/authentication/endpoint/InitVariables.java
+++ components/authentication-endpoint/src/main/java/com/wso2telco/identity/application/authentication/endpoint/InitVariables.java
@@ -2,10 +2,18 @@ package com.wso2telco.identity.application.authentication.endpoint;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.jsp.jstl.core.Config;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.wso2telco.core.config.ConfigLoader;
 import com.wso2telco.core.config.DataHolder;
 
 public class InitVariables implements ServletContextListener {
+
+	private static Log log = LogFactory.getLog(InitVariables.class);
+	private static String locale = null;
 
 	@Override
 	public void contextDestroyed(final ServletContextEvent event) {
@@ -13,11 +21,21 @@ public class InitVariables implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(final ServletContextEvent event) {
+		try {
+			if (DataHolder.getInstance().getMobileConnectConfig() == null) {
+				DataHolder.getInstance().setMobileConnectConfig(
+						ConfigLoader.getInstance().getMobileConnectConfig());
+			}
+			locale = DataHolder.getInstance().getMobileConnectConfig()
+					.getLocale();
 
-		final String locale_prop = "javax.servlet.jsp.jstl.fmt.locale";
-		String locale = DataHolder.getInstance().getMobileConnectConfig()
-				.getLocale();
-		System.setProperty(locale_prop, locale);
+		} catch (Exception ex) {
+			log.error("Error while reading Mobile Connect Configuration");
+
+		}
+
+		Config.set(event.getServletContext(), Config.FMT_LOCALE, locale);
 
 	}
+
 }
