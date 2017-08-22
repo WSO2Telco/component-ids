c165b7fbab97db3e783d457746b49ac6c7c036bb
aushaniU <aushani@wso2telco.com>
2017-08-22 11:07:44 +0530

    [IDSDEV-695] add constant related to scopes

--- components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/Constants.java
+++ components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/Constants.java
@@ -449,7 +449,7 @@ public final class Constants {
 
     public static final String EXPLICIT_SCOPES = "explicitScopes";
 
-    public static final String IMPLICIT_SCOPES = "explicitScopes";
+    public static final String IMPLICIT_SCOPES = "implicitScopes";
 
     public static final String LONGLIVEDSCOPES = " longlivedScopes";
 
