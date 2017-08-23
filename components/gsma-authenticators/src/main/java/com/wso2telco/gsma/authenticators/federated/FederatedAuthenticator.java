f21e063488e367ea4ff1b215def71c7196e304cd
Sheshan <seshangamage@gmail.com>
2017-08-23 19:51:00 +0530

    IDSDEV-679 [ABRT:BRAZIL] - Federated Authenticator Implementation

--- components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/federated/FederatedAuthenticator.java
+++ components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/federated/FederatedAuthenticator.java
@@ -14,7 +14,6 @@ import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.AbstractApplicationAuthenticator;
 import org.wso2.carbon.identity.application.authentication.framework.AuthenticatorFlowStatus;
 import org.wso2.carbon.identity.application.authentication.framework.LocalApplicationAuthenticator;
-import org.wso2.carbon.identity.application.authentication.framework.config.model.ApplicationConfig;
 import org.wso2.carbon.identity.application.authentication.framework.config.model.StepConfig;
 import org.wso2.carbon.identity.application.authentication.framework.context.AuthenticationContext;
 import org.wso2.carbon.identity.application.authentication.framework.exception.AuthenticationFailedException;
@@ -26,36 +25,40 @@ import org.wso2.carbon.identity.user.registration.stub.UserRegistrationAdminServ
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.rmi.RemoteException;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 public class FederatedAuthenticator extends AbstractApplicationAuthenticator
-        implements LocalApplicationAuthenticator {
+        implements LocalApplicationAuthenticator  {
 
     private static Log log = LogFactory.getLog(FederatedAuthenticator.class);
     private static final String IS_FLOW_COMPLETED = "isFlowCompleted";
     private static MobileConnectConfig mobileConnectConfig = null;
     private static ConfigurationService configurationService = new ConfigurationServiceImpl();
-    private static MobileConnectConfig.FederatedIdentityProviders federatedIdps = mobileConnectConfig
-            .getFederatedIdentityProviders();
-    private static HashMap<String, MobileConnectConfig.Provider> federatedIdpMap = new HashMap<>();
+    private static MobileConnectConfig.Provider[] providers;
     private static final String LOGIN_HINT = "login_hint";
     private static final String NONCE = "nonce";
     private static DbService dbConnection = new DbService();
+    private static final String BRAZIL_COUNTRY_CODE = "55";
+    private static final String SESSION_DATA_KEY= "sessionDataKey";
+    private static final String IDP_AOUTH_CODE = "code";
 
 
     static {
         mobileConnectConfig = configurationService.getDataHolder().getMobileConnectConfig();
+        providers = mobileConnectConfig.getFederatedIdentityProviders().getProvider();
 
     }
 
     @Override
     public AuthenticatorFlowStatus process(HttpServletRequest request, HttpServletResponse response, AuthenticationContext context) throws AuthenticationFailedException, LogoutFailedException {
 
-        log.info("Yepeeeeeeeeeeeeeeee");
+        log.info("FederatedAuthenticator process Triggered");
         if ((canHandle(request) && !triggerInitiateAuthRequest(context) && (request.getAttribute(FrameworkConstants
                 .REQ_ATTR_HANDLED) == null || !(Boolean) request.getAttribute(FrameworkConstants.REQ_ATTR_HANDLED)))) {
             try {
@@ -124,66 +127,64 @@ public class FederatedAuthenticator extends AbstractApplicationAuthenticator
         super.initiateAuthenticationRequest(request, response, context);
         String queryParams = FrameworkUtils.getQueryStringWithFrameworkContextId(context.getQueryParams(),
                 context.getCallerSessionKey(), context.getContextIdentifier());
-        ApplicationConfig applicationConfig = context.getSequenceConfig().getApplicationConfig();
         Map<String, String> paramMap = Util.createQueryParamMap(queryParams);
         String sessionKey = context.getContextIdentifier();
         String operator = (String) context.getProperty(Constants.OPERATOR);
-        String acrValue = paramMap.get(Constants.ACR);
-        String federatedAouthEndpoint = federatedIdpMap.get(operator).getAuthzEndpoint();
-        String federatedCallBackUrl = mobileConnectConfig.getFederatedCallbackUrl();
-        String client_id = paramMap.get(Constants.CLIENT_ID);
+        String acrValue = paramMap.get(Constants.PARAM_ACR);
+        String federatedAouthEndpoint = providers[0].getAuthzEndpoint();
+        String federatedCallBackUrl = null;
+        try {
+            federatedCallBackUrl = URLEncoder.encode(mobileConnectConfig.getFederatedCallbackUrl(),
+                    String.valueOf(StandardCharsets.UTF_8));
+        } catch (UnsupportedEncodingException e) {
+            log.error(e.getMessage());
+        }
+        String clientId = paramMap.get(Constants.CLIENT_ID);
         String nonce = paramMap.get(NONCE);
         String federatedMobileConnectCallUrl=null;
 
-        log.info("~~~"+sessionKey +"~~~"+operator+"~~~"+acrValue+"~~~"+federatedAouthEndpoint+"~~~"+federatedCallBackUrl+"~~~"+client_id+"~~~"+nonce);
+        log.debug("sessionKey :"+sessionKey +" ~ operator : "+operator+" ~ acrValue : "+acrValue+" ~ federatedAouthEndpoint : "+federatedAouthEndpoint+" ~ federatedCallBackUrl : "+federatedCallBackUrl+" ~ client_id : "+clientId+" ~ nonce : "+nonce);
         if(paramMap.containsKey(LOGIN_HINT)){
-            String login_hint = paramMap.get(LOGIN_HINT);
-            federatedMobileConnectCallUrl = federatedAouthEndpoint +"?response_type=code&state="+sessionKey+"&nonce="+nonce+"&max_age=3600&scope=openid&response_type=code&redirect_uri="+federatedCallBackUrl+"&client_id="+client_id+"&acr_values="+acrValue+"&login_hint="+login_hint;
+            String loginHint = paramMap.get(LOGIN_HINT);
+            federatedMobileConnectCallUrl = federatedAouthEndpoint +"?response_type=code&state="+sessionKey+"&nonce="+nonce+"&max_age=3600&scope=openid&response_type=code&redirect_uri="+federatedCallBackUrl+"&client_id="+clientId+"&acr_values="+acrValue+"&login_hint="+loginHint;
         }else{
-            federatedMobileConnectCallUrl = federatedAouthEndpoint +"?response_type=code&state="+sessionKey+"&nonce="+nonce+"&max_age=3600&scope=openid&response_type=code&redirect_uri="+federatedCallBackUrl+"&client_id="+client_id+"&acr_values="+acrValue;
+            federatedMobileConnectCallUrl = federatedAouthEndpoint +"?response_type=code&state="+sessionKey+"&nonce="+nonce+"&max_age=3600&scope=openid&response_type=code&redirect_uri="+federatedCallBackUrl+"&client_id="+clientId+"&acr_values="+acrValue;
         }
 
         try {
-            log.info("~~~~~~~~~~~~~~~~"+federatedMobileConnectCallUrl);
-            context.setProperty(Constants.MSISDN , generateRandomNumber("55"));
-            log.info("~~~~~~~~~~~~~~~~"+context.getProperty(Constants.MSISDN));
+            log.debug(" ~ federatedMobileConnectCallUrl : "+federatedMobileConnectCallUrl);
+            context.setProperty(Constants.MSISDN , generateRandomNumber(BRAZIL_COUNTRY_CODE));
+            log.debug(" ~ MSISDN : "+context.getProperty(Constants.MSISDN));
             context.setProperty(IS_FLOW_COMPLETED, false);
             response.sendRedirect(federatedMobileConnectCallUrl);
 
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("Error Calling IDP Aouth Url : " + e.getMessage());
         }
     }
     @Override
     protected void processAuthenticationResponse(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationContext authenticationContext) throws AuthenticationFailedException {
-        log.info("Tagaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        String queryParams = FrameworkUtils.getQueryStringWithFrameworkContextId(authenticationContext.getQueryParams(),
-                authenticationContext.getCallerSessionKey(), authenticationContext.getContextIdentifier());
-        ApplicationConfig applicationConfig = authenticationContext.getSequenceConfig().getApplicationConfig();
-        Map<String, String> paramMap = Util.createQueryParamMap(queryParams);
-        String applicationName = applicationConfig.getApplicationName();
-        String sessionKey = authenticationContext.getCallerSessionKey();
-        String federatedOuthCode = httpServletRequest.getParameter("code");
-        log.info("~~~~~~~~~~~"+federatedOuthCode);
+        log.info("FederatedAuthenticator process Authentication Response Triggered");
+        String federatedOuthCode = httpServletRequest.getParameter(IDP_AOUTH_CODE);
+        log.debug("Federated IDP returned AouthCode ~ "+federatedOuthCode);
         authenticationContext.setProperty(IS_FLOW_COMPLETED, true);
-        log.info("~~~~~~~~~~~~~~~~"+authenticationContext.getProperty(Constants.MSISDN));
+        log.debug("~ MSISDN : "+authenticationContext.getProperty(Constants.MSISDN));
         String operator = (String) authenticationContext.getProperty(Constants.OPERATOR);
         try {
-            new UserProfileManager().createUserProfileLoa2(authenticationContext.getProperty(Constants.MSISDN).toString(), operator, Constants.SCOPE_MNV);
+            new UserProfileManager().createUserProfileLoa2(authenticationContext.getProperty(Constants.MSISDN).toString(), operator, Constants.OPENID);
         } catch (UserRegistrationAdminServiceIdentityException e) {
-            e.printStackTrace();
+            log.error("Error Creating User Profile User Registration Admin Service : " + e.getMessage());
         } catch (RemoteException e) {
-            e.printStackTrace();
+            log.error("Error Creating User Profile : " + e.getMessage());
         }
-        //AuthenticatedUser.createFederateAuthenticatedUserFromSubjectIdentifier(authenticationContext.getProperty(Constants.MSISDN).toString());
+
         try {
-            dbConnection.insertFederatedAuthCodeMappings(operator, null, federatedOuthCode);
+            dbConnection.insertFederatedAuthCodeMappings(operator,federatedOuthCode);
         } catch (Exception e) {
-            e.printStackTrace();
-            log.info("Error Persisting Federdeated Aouth Code");
+            log.error("Error Persisting Federdeated Aouth Code : " + e.getMessage());
         }
         AuthenticationContextHelper.setSubject(authenticationContext, authenticationContext.getProperty(Constants.MSISDN).toString());
-        log.info("Authentication success");
+        log.info("FederatedAuthenticator Authentication success");
 
     }
 
@@ -199,17 +200,17 @@ public class FederatedAuthenticator extends AbstractApplicationAuthenticator
 
     @Override
     public String getContextIdentifier(HttpServletRequest httpServletRequest) {
-        return httpServletRequest.getParameter("sessionDataKey");
+        return httpServletRequest.getParameter(SESSION_DATA_KEY);
     }
 
     @Override
     public String getName() {
-        return Constants.FEDERATED_AUTHENTICATOR_NAME.toString();
+        return Constants.FEDERATED_AUTHENTICATOR_NAME;
     }
 
     @Override
     public String getFriendlyName() {
-        return Constants.FEDERATED_AUTHENTICATOR_FRIENDLY_NAME.toString();
+        return Constants.FEDERATED_AUTHENTICATOR_FRIENDLY_NAME;
     }
 
 
@@ -218,9 +219,10 @@ public class FederatedAuthenticator extends AbstractApplicationAuthenticator
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         long mills = calendar.getTimeInMillis();
-        long modulesvalue = mills%10000000000L;
-        String msisdn = "55"+String.valueOf(modulesvalue);
-        return msisdn;
+        long modulesValue = mills%10000000000L;
+        return countryCode + String.valueOf(modulesValue);
+
     }
+
 
 }
