9e47ddbf69736923b6c35ffcfc2e2f198487edfa
aushaniU <aushani@wso2telco.com>
2017-08-31 17:08:20 +0530

    [IDSDEV-702] added classes related to validation layer

---
+++ components/auth-proxy-service/src/main/java/com/wso2telco/proxy/attributeShare/AttributeShare.java
@@ -0,0 +1,16 @@
+package com.wso2telco.proxy.attributeShare;
+
+/**
+ * Created by aushani on 8/31/17.
+ */
+public class AttributeShare {
+
+    public void validateAttShareDetails(){
+
+        //scope
+
+
+
+
+    }
+}
---
+++ components/auth-proxy-service/src/main/java/com/wso2telco/proxy/attributeShare/ScopeFactory.java
@@ -0,0 +1,15 @@
+package com.wso2telco.proxy.attributeShare;
+
+/**
+ * Created by aushani on 8/31/17.
+ */
+public class ScopeFactory {
+
+    public static AttrubteSharable getAttribAttrubteSharable() {
+
+        AttrubteSharable attrubteSharable = null;
+
+        return attrubteSharable;
+
+    }
+}
