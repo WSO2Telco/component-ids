3075e69ce074bd29a5c0d9b4cc665681ec700df7
nilan <nilan.rukshan@gmail.com>
2017-09-15 16:47:44 +0530

    IDSDEV-659

--- components/user-claim-retriever/src/main/java/com/wso2telco/claims/RemoteClaimsRetriever.java
+++ components/user-claim-retriever/src/main/java/com/wso2telco/claims/RemoteClaimsRetriever.java
@@ -3,6 +3,7 @@ package com.wso2telco.claims;
 import com.wso2telco.core.config.model.MobileConnectConfig;
 import com.wso2telco.core.config.model.ScopeDetailsConfig;
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -23,16 +24,14 @@ public class RemoteClaimsRetriever implements ClaimsRetriever {
 
     private static Map<String, ScopeDetailsConfig.Scope> scopeConfigsMap = new HashMap<String, ScopeDetailsConfig.Scope>();
 
-    private final String phone_number_claim = "phone_number";
+    private final static String PHONE_NUMBER = "phone_number";
 
-    private final String operator = "operator1";
+    private final static String OPERATOR = "operator1";
 
     private static List<MobileConnectConfig.OperatorData> operators = com.wso2telco.core.config.ConfigLoader.getInstance().getMobileConnectConfig().getOperatorsList().getOperatorData();
 
 
-    @Override
-    public Map<String, Object> getRequestedClaims(String[] scopes, List<ScopeDetailsConfig.Scope> scopeConfigs, Map<String, Object> totalClaims) throws NoSuchAlgorithmException {
-        Map<String, Object> requestedClaims = new HashMap<String, Object>();
+    private void populateScopeConfigs(List<ScopeDetailsConfig.Scope> scopeConfigs) {
         if (!scopeConfigsMap.isEmpty()) {
             for (ScopeDetailsConfig.Scope scope : scopeConfigs) {
                 scopeConfigsMap.put(scope.getName(), scope);
@@ -42,23 +41,34 @@ public class RemoteClaimsRetriever implements ClaimsRetriever {
                 log.debug("Could not load user-info claims.");
             }
         }
+    }
 
-        String operatorName = (String) totalClaims.get(operator);
-        String phoneNumber = (String) totalClaims.get(phone_number_claim);
+    private String getclaimValue(Iterator<String> i, boolean isHashed, Map<String, Object> totalClaims) throws NoSuchAlgorithmException {
+        String claimValue = "";
+        while (i.hasNext()) {
+            Object claimStr = totalClaims.get(i.next());
+            if (claimStr != null) {
+                claimValue = (isHashed) ? getHashedClaimValue(totalClaims.get(i.next()).toString()) : totalClaims.get(i.next()).toString();
+
+            }
+        }
+        return claimValue;
+    }
+
+    @Override
+    public Map<String, Object> getRequestedClaims(String[] scopes, List<ScopeDetailsConfig.Scope> scopeConfigs, Map<String, Object> totalClaims) throws NoSuchAlgorithmException {
+        Map<String, Object> requestedClaims = new HashMap<String, Object>();
+
+        populateScopeConfigs(scopeConfigs);
+        String operatorName = (String) totalClaims.get(OPERATOR);
+        String phoneNumber = (String) totalClaims.get(PHONE_NUMBER);
         totalClaims = getTotalClaims(operatorName, phoneNumber);
 
         for (String scope : scopes) {
-            if (scopeConfigsMap.containsKey(scope)) {
+            if (scopeConfigsMap.containsKey(scope) && totalClaims != null) {
                 Iterator<String> i = scopeConfigsMap.get(scope).getClaimSet().iterator();
                 boolean isHashed = scopeConfigsMap.get(scope).isHashed();
-                while (i.hasNext()) {
-                    Object claimStr = totalClaims.get(i.next());
-                    if (claimStr != null) {
-                        String claimValue = (isHashed) ? getHashedClaimValue(totalClaims.get(i.next()).toString()) : totalClaims.get(i.next()).toString();
-                        requestedClaims.put(i.next(), totalClaims.get(claimValue));
-                    }
-
-                }
+                requestedClaims.put(i.next(), totalClaims.get(getclaimValue(i, isHashed, totalClaims)));
 
             }
         }
@@ -101,7 +111,7 @@ public class RemoteClaimsRetriever implements ClaimsRetriever {
 
         byte byteData[] = md.digest();
         //convert the byte to hex format
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < byteData.length; i++) {
             sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
         }
