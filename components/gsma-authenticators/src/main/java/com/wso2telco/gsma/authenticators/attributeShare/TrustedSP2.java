cd19a6d61059e5117a32cce7ab04f6073fe3c5d2
aushaniU <aushani@wso2telco.com>
2017-10-02 18:46:42 +0530

    [IDSDEV-723] Fixed issue related with tsp2 registration

--- components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/attributeShare/TrustedSP2.java
+++ components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/attributeShare/TrustedSP2.java
@@ -5,12 +5,16 @@ import com.wso2telco.gsma.authenticators.dao.AttributeConfigDAO;
 import com.wso2telco.gsma.authenticators.dao.impl.AttributeConfigDAOimpl;
 import com.wso2telco.gsma.authenticators.model.SPConsent;
 import com.wso2telco.gsma.authenticators.model.UserConsentHistory;
+import com.wso2telco.gsma.authenticators.util.AuthenticationContextHelper;
+import com.wso2telco.gsma.authenticators.util.UserProfileManager;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.context.AuthenticationContext;
 import org.wso2.carbon.identity.application.authentication.framework.exception.AuthenticationFailedException;
+import org.wso2.carbon.identity.user.registration.stub.UserRegistrationAdminServiceIdentityException;
 
 import javax.naming.NamingException;
+import java.rmi.RemoteException;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -28,11 +32,15 @@ public class TrustedSP2 extends AbstractAttributeShare {
         String displayScopes = "";
         String isDisplayScope = "false";
         String authenticationFlowStatus="false";
+        String isTNCForNewUser="false";
 
 
         Map<String, List<String>> attributeset = getAttributeMap(context);
         Map<String,String> attributeShareDetails = new HashMap();
         boolean isRegistering = (boolean) context.getProperty(Constants.IS_REGISTERING);
+        String msisdn = context.getProperty(Constants.MSISDN).toString();
+        String operator = context.getProperty(Constants.OPERATOR).toString();
+        boolean isAttributeScope = (Boolean)context.getProperty(Constants.IS_ATTRIBUTE_SHARING_SCOPE);
 
 
         if(!attributeset.get(Constants.EXPLICIT_SCOPES).isEmpty()){
@@ -41,10 +49,24 @@ public class TrustedSP2 extends AbstractAttributeShare {
             log.debug("Found the explicite scopes to gt the consent" + displayScopes );
         }
 
+        try {
+            if(isRegistering){
+                int requestedLoa = Integer.parseInt(context.getProperty(Constants.ACR).toString());
+                if(requestedLoa == 2){
+                    new UserProfileManager().createUserProfileLoa2(msisdn, operator,isAttributeScope);
+                }
+                AuthenticationContextHelper.setSubject(context, context.getProperty(Constants.MSISDN).toString());
+                context.setProperty(Constants.TERMINATE_BY_REMOVE_FOLLOWING_STEPS, "true");
+                context.setProperty(Constants.AUTHENTICATED_USER,"true");
+            }
+        } catch (RemoteException | UserRegistrationAdminServiceIdentityException e) {
+            throw new AuthenticationFailedException(e.getMessage(), e);
+        }
         context.setProperty(Constants.IS_CONSENTED,Constants.YES);
         attributeShareDetails.put(Constants.IS_DISPLAYSCOPE,isDisplayScope);
         attributeShareDetails.put(Constants.IS_AUNTHENTICATION_CONTINUE,authenticationFlowStatus);
         attributeShareDetails.put(Constants.DISPLAY_SCOPES,displayScopes);
+        attributeShareDetails.put(Constants.IS_TNC,isTNCForNewUser);
 
         return attributeShareDetails;
 
