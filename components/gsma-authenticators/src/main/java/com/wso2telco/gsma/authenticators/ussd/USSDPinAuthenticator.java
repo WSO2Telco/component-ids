66220e25a08de2de687abfafeee8af916ca2722b
lakini <lakini@wso2telco.com>
2017-11-16 12:34:38 +0530

    [IDSDEV-772] Fixed the bug - Not updating LOA level 2 to LOA level 3 when login from a main scope with LOA3 request

--- components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/ussd/USSDPinAuthenticator.java
+++ components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/ussd/USSDPinAuthenticator.java
@@ -339,11 +339,11 @@ public class USSDPinAuthenticator extends AbstractApplicationAuthenticator
         }
 
         try {
-            if (isRegistering || isStatusUpdate) {
+            if (isRegistering) {
                 handleUserRegistration(context, userStatus);
             } else {
                 if (isProfileUpgrade) {
-                    handleProfileUpgrade(context);
+                    handleProfileUpgrade(context,isStatusUpdate);
                 } else if (isPinReset) {
                     retryAuthenticatorForPinReset(context);
                 } else if (isPinResetConfirmation) {
@@ -542,7 +542,7 @@ public class USSDPinAuthenticator extends AbstractApplicationAuthenticator
         return pinConfig.getCurrentStep() == PinConfig.CurrentStep.PIN_RESET;
     }
 
-    private void handleProfileUpgrade(AuthenticationContext context) throws
+    private void handleProfileUpgrade(AuthenticationContext context,boolean isStatusUpdate) throws
             RemoteUserStoreManagerServiceUserStoreExceptionException, RemoteException, UnsupportedEncodingException,
             NoSuchAlgorithmException, AuthenticationFailedException {
         boolean securityQuestionsShown = context.getProperty(Constants.IS_SECURITY_QUESTIONS_SHOWN) != null &&
@@ -556,6 +556,9 @@ public class USSDPinAuthenticator extends AbstractApplicationAuthenticator
             String challengeQuestion1 = (String) context.getProperty(Constants.CHALLENGE_QUESTION_1);
             String challengeQuestion2 = (String) context.getProperty(Constants.CHALLENGE_QUESTION_2);
             String msisdn = (String) context.getProperty(Constants.MSISDN);
+            boolean isAttributeScope = (Boolean)context.getProperty(Constants.IS_ATTRIBUTE_SHARING_SCOPE);
+            String spType = context.getProperty(Constants.TRUSTED_STATUS).toString();
+            String attrShareType = context.getProperty(Constants.ATTRSHARE_SCOPE_TYPE).toString();
             PinConfig pinConfig = (PinConfig) context.getProperty(com.wso2telco.core.config.util.Constants
                     .PIN_CONFIG_OBJECT);
 
@@ -571,7 +574,7 @@ public class USSDPinAuthenticator extends AbstractApplicationAuthenticator
             challengeAnswer2 = challengeQuestion2 + Constants.USER_CHALLENGE_SEPARATOR + challengeAnswer2;
 
             new UserProfileManager().updateUserProfileForLOA3(challengeAnswer1, challengeAnswer2, pinConfig
-                    .getConfirmedPin(), msisdn);
+                    .getConfirmedPin(), msisdn ,isStatusUpdate,isAttributeScope,spType,attrShareType);
         } else {
             context.setProperty(Constants.IS_SECURITY_QUESTIONS_ANSWERED, Boolean.TRUE);
 
