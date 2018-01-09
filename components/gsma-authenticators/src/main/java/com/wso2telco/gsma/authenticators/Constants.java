57d94b2de6c9290db19df5766ae613ab081d4877
lakini <lakinisenanayaka15@gmail.com>
2018-01-09 22:33:46 +0530

    [IDSDEV-817] Product Building fail Fixed

--- components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/Constants.java
+++ components/gsma-authenticators/src/main/java/com/wso2telco/gsma/authenticators/Constants.java
@@ -251,8 +251,6 @@ public final class Constants {
 
     public static final String PIN_REGISTRATION_JSP = "/mcx-user-registration/auth_pin_registration";
 
-    public static final String SAA_WAITING_JSP = "/mcx-user-registration/auth_registration_saa_waiting";
-
     public static final String REDIRECT_URI = "redirectURI";
 
     public static final String COMMON_AUTH_HANDLED = "commonAuthHandled";
